bbc370608a33618121653906c828ef4a48e651f9
Zero <tobewhatwewant@gmail.com>
2023-09-05 21:10:18 +0800

    fix: lint

--- keyword.go
+++ keyword.go
@@ -6,10 +6,12 @@ import (
 	"github.com/go-zoox/fetch"
 )
 
+// KeywordOptions is the options for keyword health check
 type KeywordOptions struct {
 	HTTPOptions
 }
 
+// Keyword checks if the given url is healthy (reachable) via HTTP and contains the given keyword
 func Keyword(url, keyword string, opts ...*KeywordOptions) (bool, error) {
 	optx := HTTPOptions{}
 	if len(opts) > 0 && opts[0] != nil {
